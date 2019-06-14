aa9a7d3911dec0b59318f8f882d0a7a012279003
Peter Tyldesley <ptylde@gmail.com>
2019-06-14 18:47:52 +0100

    Add browser router and remove app.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Grommet } from "grommet";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import theme from "config/theme";
 import AppBar from "components/AppBar";
 import Dashboard from "pages/Dashboard";
 
 function App() {
   return (
-    <Grommet full theme={theme}>
-      <AppBar />
-      <Dashboard />
-    </Grommet>
+    <Router>
+      <Grommet full theme={theme}>
+        <AppBar />
+        <Route exact path="/" component={Dashboard} />
+      </Grommet>
+    </Router>
   );
 }
 
